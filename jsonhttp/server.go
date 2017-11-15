e5c3bbe7d9bfb0546294e7d01b7a2709dbd6a97e
Tom Wilkie <tom.wilkie@gmail.com>
2017-11-15 14:45:24 +0000

    Add metrics to HTTP server.

--- jsonhttp/server.go
+++ jsonhttp/server.go
@@ -4,10 +4,34 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/felixge/httpsnoop"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
+var requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "request_duration_seconds",
+	Help:    "Time (in seconds) spent serving HTTP requests.",
+	Buckets: prometheus.DefBuckets,
+}, []string{"method", "route", "status_code"})
+
+func init() {
+	prometheus.MustRegister(requestDuration)
+}
+
+func wrap(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m := httpsnoop.CaptureMetrics(h, w, r)
+		requestDuration.WithLabelValues(r.Method, r.URL.Path,
+			strconv.Itoa(m.Code)).Observe(m.Duration.Seconds())
+	})
+}
+
 func server(addr string) {
-	http.Handle("/greeter", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/metrics", prometheus.Handler())
+
+	http.Handle("/greeter", wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var req request
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -22,6 +46,6 @@ func server(addr string) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-	}))
+	})))
 	log.Fatalf("listen err: %v", http.ListenAndServe(addr, nil))
 }
